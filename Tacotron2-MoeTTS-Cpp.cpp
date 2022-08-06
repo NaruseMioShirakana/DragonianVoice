5e2ed826a572dcbd1e63a3fb442348d17bc29642
藤原白叶 <40709280+FujiwaraShirakana@users.noreply.github.com>
2022-08-06 21:52:13 +0800

    Delete Tacotron2-MoeTTS-Cpp.cpp

--- Tacotron2-MoeTTS-Cpp.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <QApplication>
-#include "ShirakanaUI.h"
-#pragma execution_character_set("utf-8")
-int main(int argc, char* argv[]) {
-	QApplication app(argc, argv);
-	ShirakanaUI shirakanaMainWindow;
-	shirakanaMainWindow.show();
-	return app.exec();
-}
