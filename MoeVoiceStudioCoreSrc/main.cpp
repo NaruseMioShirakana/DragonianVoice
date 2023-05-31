8b817d25344e17bbaa3dee191a85d1d2e07d3873
Sine Striker <trueful@163.com>
2023-05-31 15:18:46 +0800

    Add qt translation support

--- MoeVoiceStudioCoreSrc/main.cpp
+++ MoeVoiceStudioCoreSrc/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString& locale : uiLanguages) {
         const QString baseName = "MoeVS_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
+        if (translator.load(QString(":/localization/%1.qm").arg(baseName))) {
             app.installTranslator(&translator);
             break;
         }
